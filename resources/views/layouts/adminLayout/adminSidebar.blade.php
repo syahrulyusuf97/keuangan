1d86c314a0573d9c6368449264d4dac50bfc64de
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-02-05 17:48:39 +0700

    menu laporan

--- resources/views/layouts/adminLayout/adminSidebar.blade.php
+++ resources/views/layouts/adminLayout/adminSidebar.blade.php
@@ -45,6 +45,19 @@
           <i class="fa fa-money"></i> <span>Kas Keluar</span>
         </a>
       </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-pie-chart"></i>
+          <span>Laporan</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa fa-bar-chart"></i> Chart</a></li>
+          <li><a href="#"><i class="fa fa-list"></i> Arus Kas/<i>Cashflow</i></a></li>
+        </ul>
+      </li>
     </ul>
   </section>
   <!-- /.sidebar -->
