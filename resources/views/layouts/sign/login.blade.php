8aeb7b75818517ebe4dfbbd1260ab39b585db694
syahrulyusuf97 <syahrulyusuf97@gmail.com>
2019-04-24 09:25:13 +0700

    UI login

--- resources/views/layouts/sign/login.blade.php
+++ resources/views/layouts/sign/login.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="{{ asset('public/images/icon/keuanganku.png') }}" />
-
+    <title>KeuanganKu | @yield('title')</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
--- resources/views/sign/login.blade.php
+++ resources/views/sign/login.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.sign.login')
+@section('title', 'Login')
 @section('content')
 
 <div class="login-box">
